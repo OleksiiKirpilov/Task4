085ce976024c4e246a3e4429949f3725fa28706c
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-07-13 20:30:04 +0300

    part 1 test

--- src/main/java/com/epam/rd/java/basic/practice4/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part1.java
@@ -1,9 +1,52 @@
 package com.epam.rd.java.basic.practice4;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Part1 {
 
-    public static void main(String[] args) {
+    private static final Logger LOGGER = Logger.getGlobal();
 
+    public static Logger getLogger() {
+        return LOGGER;
+    }
+
+    public static String getInput(String fileName) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            Scanner scanner = new Scanner(new File(fileName), "cp1251");
+            while (scanner.hasNextLine()) {
+                sb.append(scanner.nextLine()).append(System.lineSeparator());
+            }
+            scanner.close();
+            return sb.toString().trim();
+        } catch (IOException ex) {
+            getLogger().log(Level.SEVERE, ex.getMessage(), ex);
+        }
+        return sb.toString();
+    }
+
+    public static String process(String input) {
+        Matcher m = Pattern.compile("[A-zА-яёЁіІ]{4,}").matcher(input);
+        StringBuilder sb = new StringBuilder(input);
+        int deletedCount = 0;
+        while (m.find()) {
+            int index = m.start() - deletedCount;
+            sb.delete(index, index + 2);
+            deletedCount += 2;
+        }
+        return sb.toString();
+
+    }
+
+    public static void main(String[] args) {
+        String input = getInput("part1.txt");
+        System.out.println(process(input));
     }
 
 }
