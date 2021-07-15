ed89e80203d38887fffd2f14be64242af30a7c0f
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-07-15 22:43:24 +0300

    part 4 fix

--- src/main/java/com/epam/rd/java/basic/practice4/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part4.java
@@ -16,6 +16,8 @@ public class Part4 implements Iterable<String> {
 
     public static void main(String[] args) {
         String input = Part1.getInput("part4.txt");
+        String[] lines = input.split(System.lineSeparator());
+        input = String.join("", lines);
         Part4 t = new Part4(input);
         for (String s : t) {
             System.out.print(s);
