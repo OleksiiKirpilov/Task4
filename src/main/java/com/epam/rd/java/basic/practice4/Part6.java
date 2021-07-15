ec81f88a9bcc678c7a4f7f3b81d390ab61c2e221
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-07-15 23:18:04 +0300

    part 6 test

--- src/main/java/com/epam/rd/java/basic/practice4/Part6.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part6.java
@@ -1,9 +1,40 @@
 package com.epam.rd.java.basic.practice4;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Part6 {
 
     public static void main(String[] args) {
-        //stub
+        String data = Part1.getInput("part6.txt");
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            switch (in.nextLine()) {
+                case "stop":
+                    return;
+                case "cyrl":
+                    show(data, "\\p{IsCyrillic}+");
+                    break;
+                case "latn":
+                    show(data, "\\p{IsLatin}+");
+                    break;
+                default:
+                    System.out.println("Incorrect input");
+            }
+        }
+    }
+
+    private static void show(String data, String regex) {
+        Matcher m = Pattern.compile(regex, Pattern.UNICODE_CHARACTER_CLASS).matcher(data);
+        StringBuilder sb = new StringBuilder();
+        while (m.find()) {
+            sb.append(m.group()).append(' ');
+        }
+        if (sb.length() > 1) {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        System.out.print(sb);
     }
 
 }
