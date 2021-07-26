6041a4e41702dee442f1bbc850611b733821038b
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-07-26 19:13:48 +0300

    demo refactored

--- src/main/java/com/epam/rd/java/basic/practice4/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Demo.java
@@ -8,6 +8,12 @@ public class Demo {
 
     private static final InputStream STD_IN = System.in;
 
+    private static ByteArrayInputStream prepareMockStream(String input) {
+        return new ByteArrayInputStream(input
+                .replace("^", System.lineSeparator())
+                .getBytes(StandardCharsets.UTF_8));
+    }
+
     public static void main(String[] args) {
         System.out.println("=========================== PART1");
         Part1.main(args);
@@ -16,10 +22,7 @@ public class Demo {
         Part2.main(args);
 
         System.out.println("=========================== PART3");
-        System.setIn(new ByteArrayInputStream(
-                "char^String^int^double^stop"
-                        .replace("^", System.lineSeparator())
-                        .getBytes(StandardCharsets.UTF_8)));
+        System.setIn(prepareMockStream("char^String^int^double^stop"));
         Part3.main(args);
         System.setIn(STD_IN);
 
@@ -27,16 +30,12 @@ public class Demo {
         Part4.main(args);
 
         System.out.println("=========================== PART5");
-        System.setIn(new ByteArrayInputStream(
-                "table ru^table en^apple ru^stop".replace("^",
-                        System.lineSeparator()).getBytes(StandardCharsets.UTF_8)));
+        System.setIn(prepareMockStream("table ru^table en^apple ru^stop"));
         Part5.main(args);
         System.setIn(STD_IN);
 
         System.out.println("=========================== PART6");
-        System.setIn(new ByteArrayInputStream(
-                "Latn^Cyrl^asdf^latn^cyrl^stop".replace("^",
-                        System.lineSeparator()).getBytes(StandardCharsets.UTF_8)));
+        System.setIn(prepareMockStream("Latn^Cyrl^asdf^latn^cyrl^stop"));
         Part6.main(args);
         System.setIn(STD_IN);
     }
